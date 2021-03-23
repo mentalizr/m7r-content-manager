2c5eb5a300154026b60ea446696151894b3d53f4
Arthur Picht <picht@arthurpicht.de>
2021-03-23 21:25:03 +0100

    fix BuildException

--- src/main/java/org/mentalizr/contentManager/build/BuildException.java
+++ src/main/java/org/mentalizr/contentManager/build/BuildException.java
@@ -2,16 +2,37 @@ package org.mentalizr.contentManager.build;
 
 import org.mentalizr.contentManager.exceptions.ProgramManagerException;
 
+import java.nio.file.Path;
+
 public class BuildException extends ProgramManagerException {
 
-    private final String fileName;
+    private final Path path;
 
-    public BuildException(String fileName, String message) {
-        super("Exception on compiling file [" + fileName + "]: " + message);
-        this.fileName = fileName;
+    public BuildException(Path path) {
+        this.path = path;
     }
 
-    public String getFileName() {
-        return fileName;
+    public BuildException(Path path, String message) {
+        super(message);
+        this.path = path;
+    }
+
+    public BuildException(Path path, String message, Throwable cause) {
+        super(message, cause);
+        this.path = path;
+    }
+
+    public BuildException(Path path, Throwable cause) {
+        super(cause);
+        this.path = path;
+    }
+
+    public BuildException(Path path, String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.path = path;
+    }
+
+    public Path getPath() {
+        return this.path;
     }
 }
